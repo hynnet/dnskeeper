c7111af40d6629c9602d8fc926e4072dc24c95df
Zhou Yuntao <zhouyuntao@magictavern.com>
2015-12-16 12:39:01 +0800

    optimize query logs (for debugging)

--- dns-server.js
+++ dns-server.js
@@ -16,16 +16,28 @@ function createAnswerA(domain, ip, ttl) {
     });
 }
 
+function getIPsFromAnswer(answers) {
+    var ips = [];
+    if (!answers) {
+        return ips;
+    }
+    for (var i=0; i<answers.length; i++) {
+        if (answers[i].type==1) {
+            ips.push(answers[i].address);
+        }
+    }
+    return ips;
+}
+
 // only handle first question for now
 function handleRequest(req, callback) {
     var question = req.question[0];
-
-    console.debug('query from %s : %s', req.address.address, question.name);
 
     // query static table first
     var staticConf = config.get('static');
     var staticResult = staticConf.table[question.name];
     if (staticResult != null) {
+        console.debug('query  static  for [%s, %s]: %s', req.address.address, question.name, staticResult);
         callback(null, [createAnswerA(question.name, staticResult, staticConf.ttl)]);
         return;
     }
@@ -36,9 +48,14 @@ function handleRequest(req, callback) {
         server: domesticServer
     });
     dreq.on('timeout', function() {
+        console.debug('query  timeout for [%s, %s]', req.address.address, question.name);
         callback(null, []);
     });
     dreq.on('message', function(err, answer) {
+        if (config.get('debug')) {
+            console.debug('query answered for (%s, %s): [%s]', req.address.address, question.name,
+                getIPsFromAnswer(answer.answer));
+        }
         callback(null, answer.answer);
     });
     dreq.send();
