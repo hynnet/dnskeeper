f6f87a06be373a633a40f5f306f097c78f9592d3
HuangYingNing <huangyingning@gmail.com>
2017-10-22 15:42:26 +0800

    添加CentOS的运行环境提示、服务支持；添加gfwlist规则文件；添加默认生产配置。

--- service-dns-server.sh
+++ service-dns-server.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 export NODE_ENV=production
-path=`realpath $0`
-path=`dirname $path`
+if hash realpath 2>/dev/null; then
+	path=`realpath $0`
+	path=`dirname $path`
+else
+	echo command realpath not found!
+	echo for CentOS 6.x: rpm -ivh http://ftp.tu-chemnitz.de/pub/linux/dag/redhat/el6/en/x86_64/rpmforge/RPMS/realpath-1.17-1.el6.rf.x86_64.rpm
+	echo for CentOS 7.x: rpm -ivh http://ftp.tu-chemnitz.de/pub/linux/dag/redhat/el7/en/x86_64/rpmforge/RPMS/realpath-1.17-1.el7.rf.x86_64.rpm
+	exit
+fi
 cmd=$1
 if [ "$cmd" == "" ]; then
 	echo "Usage: $0 start|stop|restart"
@@ -9,6 +16,7 @@ if [ "$cmd" == "" ]; then
 fi
 
 mkdir -p $path/log
+mkdir -p ~/.forever/log/
 
 if [ "$cmd" == "start" ]; then
 	forever $cmd -a --workingDir "$path" -l $path/log/forever.log -o $path/log/out.log -e $path/log/err.log $path/dns-server.js
