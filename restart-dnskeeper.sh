6928d7948fe64ab7f52a5b5793527879168f5582
HuangYingNing <root@eggpb_s3.eggwifi.com>
2018-01-23 12:33:47 +0800

    add local rules files.

--- restart-dnskeeper.sh
+++ restart-dnskeeper.sh
@@ -9,8 +9,8 @@ mv log/forever.log log/forever-${time}.log
 mv log/out.log log/out-${time}.log
 ps -ef | grep node | grep 'dns-server.js' | grep -v grep | awk '{print $2}' | xargs kill 
 service named restart
-# /etc/init.d/dnskeeperd start
-NODE_ENV=production /usr/local/bin/node dns-server.js &
+/etc/init.d/dnskeeperd start
+# NODE_ENV=production /usr/local/bin/node dns-server.js &
 bzip2 log/forever-${time}.log &
 bzip2 log/out-${time}.log &
 
