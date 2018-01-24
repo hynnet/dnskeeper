8dbead54b097795c2efb892d301eb6606dedee86
HuangYingNing <root@radius2.eggwifi.com>
2018-01-24 10:07:05 +0800

    add check time out.

--- restart-dnskeeper.sh
+++ restart-dnskeeper.sh
@@ -5,9 +5,14 @@ cur_dir=`pwd`
 cd `dirname $0`
 
 time=`date +%y%m%d%H`
-mv log/forever.log log/forever-${time}.log
-mv log/out.log log/out-${time}.log
-ps -ef | grep node | grep 'dns-server.js' | grep -v grep | awk '{print $2}' | xargs kill 
+if [ -f "log/forever-${time}.log" ] || [ -f "log/forever-${time}.log.bz2" ]; then
+	time=`date +%y%m%d%H%M`
+fi
+
+/etc/init.d/dnskeeperd stop
+ps -ef | grep node | grep 'dns-server.js' | grep -v grep | awk '{print $2}' | xargs kill > /dev/null 2>&1
+mv -f log/forever.log log/forever-${time}.log
+mv -f log/out.log log/out-${time}.log
 service named restart
 /etc/init.d/dnskeeperd start
 # NODE_ENV=production /usr/local/bin/node dns-server.js &
--- start-dnskeeper.sh
+++ start-dnskeeper.sh
@@ -2,6 +2,13 @@
 # 检查并启动服务
 
 if netstat -alpn | grep ':53' | grep -e "^udp" -q ; then
+	cur_dir=`pwd`
+	cd "`dirname $0`"
+	base_dir=`pwd`
+	if sed -n "/`date -d "2 minute ago" "+%Y-%m-%d %H:%M:%S"`/,\$p" "${base_dir}/log/out.log" | grep -q "timed out" ; then
+		"${base_dir}/restart-dnskeeper.sh"
+	fi
+	cd "${cur_dir}"
 	:;
 else
 	echo start...
