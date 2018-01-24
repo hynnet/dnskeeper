ecf1edb6554bbea4929510f56a0ca5c8701c925a
HuangYingNing <root@radius2.eggwifi.com>
2018-01-24 10:33:32 +0800

    fix start shell.

--- start-dnskeeper.sh
+++ start-dnskeeper.sh
@@ -5,8 +5,10 @@ if netstat -alpn | grep ':53' | grep -e "^udp" -q ; then
 	cur_dir=`pwd`
 	cd "`dirname $0`"
 	base_dir=`pwd`
-	if sed -n "/`date -d "2 minute ago" "+%Y-%m-%d %H:"`/,\$p" "${base_dir}/log/out.log" | grep -q "timed out" ; then
-		"${base_dir}/restart-dnskeeper.sh"
+	if [ -f "${base_dir}/log/out.log" ]; then
+		if sed -n "/`date -d "2 minute ago" "+%Y-%m-%d %H:"`/,\$p" "${base_dir}/log/out.log" | grep -q "timed out" ; then
+			"${base_dir}/restart-dnskeeper.sh"
+		fi
 	fi
 	cd "${cur_dir}"
 	:;
