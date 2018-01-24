8a6bdc0a027c794b2edb2a6fd76ae7c83c446e1a
HuangYingNing <root@radius2.eggwifi.com>
2018-01-24 10:25:47 +0800

    fix check time out.

--- start-dnskeeper.sh
+++ start-dnskeeper.sh
@@ -5,7 +5,7 @@ if netstat -alpn | grep ':53' | grep -e "^udp" -q ; then
 	cur_dir=`pwd`
 	cd "`dirname $0`"
 	base_dir=`pwd`
-	if sed -n "/`date -d "2 minute ago" "+%Y-%m-%d %H:%M:%S"`/,\$p" "${base_dir}/log/out.log" | grep -q "timed out" ; then
+	if sed -n "/`date -d "2 minute ago" "+%Y-%m-%d %H:"`/,\$p" "${base_dir}/log/out.log" | grep -q "timed out" ; then
 		"${base_dir}/restart-dnskeeper.sh"
 	fi
 	cd "${cur_dir}"
